6756059f68187ec9b38928329469dd55fdd6a0e3
Simon Petrac <pfoerdie@gmail.com>
2021-02-24 11:42:39 +0100

    added compatibility error until next update

--- src/module.persistence.neo4j.js
+++ src/module.persistence.neo4j.js
@@ -1,13 +1,15 @@
+throw new Error('module.persistence.neo4j : not compatible with current module.persistence');
+
 const
-	// dataFactory = require('../../module.persistence/src/module.persistence.js'),
-	// datasetFactory = require('../../module.persistence.inmemory/src/module.persistence.inmemory.js'),
-	Neo4jStore = require('./Neo4jStore.js');
+    // dataFactory = require('../../module.persistence/src/module.persistence.js'),
+    // datasetFactory = require('../../module.persistence.inmemory/src/module.persistence.inmemory.js'),
+    Neo4jStore = require('./Neo4jStore.js');
 
 /**
  * @param {NamedNode} graph
  * @para {Neo4jDriver} driver
  * @returns {Neo4jStore}
  */
-exports.store = function(graph, driver) {
-	return new Neo4jStore(graph, driver);
+exports.store = function (graph, driver) {
+    return new Neo4jStore(graph, driver);
 };
