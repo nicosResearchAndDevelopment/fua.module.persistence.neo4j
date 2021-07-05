fecb8314911b3d29289269f8c7b850c8a2af5943
Simon Petrac <pfoerdie@gmail.com>
2021-07-05 12:30:59 +0200

    updated neo4j-store still work to do, still problems to solve

---
+++ src/module.persistence.neo4j.js
@@ -0,0 +1,91 @@
+const
+    _                    = require('./module.persistence.neo4j.util.js'),
+    {Dataset, DataStore} = require('@nrd/fua.module.persistence'),
+    neo4j                = require('neo4j-driver'),
+    Query_size           = _.loadQuery('neo4j.size.cyp'),
+    Query_addRelation    = _.loadQuery('neo4j.addRelation.cyp'),
+    Query_addLiteral     = _.loadQuery('neo4j.addLiteral.cyp'),
+    Query_delete         = _.loadQuery('neo4j.delete.cyp'),
+    Query_hasRelation    = _.loadQuery('neo4j.hasRelation.cyp'),
+    Query_hasLiteral     = _.loadQuery('neo4j.hasLiteral.cyp'),
+    Query_matchRelation  = _.loadQuery('neo4j.matchRelation.cyp'),
+    Query_matchLiteral   = _.loadQuery('neo4j.matchLiteral.cyp');
+
+class Neo4jStore extends DataStore {
+
+    #driver = null;
+
+    constructor(options, factory) {
+        super(options, factory);
+        // TODO
+        _.assert(options.driver && typeof options.driver.session === 'function', 'invalid driver');
+        this.#driver = options.driver;
+    } // Neo4jStore#constructor
+
+    async size() {
+        const records = await Query_size(this.#driver);
+        return records.length > 0 && records[0].size.toNumber() || 0;
+    } // Neo4jStore#size
+
+    async match(subject, predicate, object, graph) {
+        const dataset = await super.match(subject, predicate, object, graph);
+        // TODO
+        _.assert(false, 'not implemented');
+    } // Neo4jStore#match
+
+    async add(quads) {
+        const quadArr = await super.add(quads), ts = _.hrt();
+        let addCount  = 0;
+
+        await Promise.all(quadArr.map(async (quad) => {
+            const
+                Query_add = this.factory.isLiteral(quad.object) ? Query_addLiteral : Query_addRelation,
+                records   = await Query_add(this.#driver, {
+                    subject:   quad.subject,
+                    predicate: quad.predicate,
+                    object:    quad.object,
+                    ts:        ts
+                });
+
+            if (records.length > 0 && records[0].created) {
+                addCount++;
+                this.emit('created', quad);
+            }
+        }));
+
+        return addCount;
+    } // Neo4jStore#add
+
+    async addStream(stream) {
+        const quadStream = await super.addStream(stream);
+        // TODO
+        _.assert(false, 'not implemented');
+    } // Neo4jStore#addStream
+
+    async delete(quads) {
+        const quadArr = await super.delete(quads);
+        // TODO
+        _.assert(false, 'not implemented');
+    } // Neo4jStore#delete
+
+    async deleteStream(stream) {
+        const quadStream = await super.deleteStream(stream);
+        // TODO
+        _.assert(false, 'not implemented');
+    } // Neo4jStore#deleteStream
+
+    async deleteMatches(subject, predicate, object, graph) {
+        await super.deleteMatches(subject, predicate, object, graph);
+        // TODO
+        _.assert(false, 'not implemented');
+    } // Neo4jStore#deleteMatches
+
+    async has(quads) {
+        const quadArr = await super.has(quads);
+        // TODO
+        _.assert(false, 'not implemented');
+    } // Neo4jStore#has
+
+} // Neo4jStore
+
+module.exports = Neo4jStore;
---
+++ src/module.persistence.neo4j.util.js
@@ -0,0 +1,88 @@
+const {
+          Assert,
+          lockProp,
+          isDefined, isTruthy, isFalsy,
+          isBoolean, isNumber, isString,
+          isFunction, isObject,
+          isArray, isIterable
+      }                   = require('@nrd/fua.core.util'),
+      uuid                = require('@nrd/fua.core.uuid'),
+      hrt                 = require('@nrd/fua.core.hrt'),
+      neo4j               = require('neo4j-driver'),
+      {join: joinPath}    = require('path'),
+      {readFileSync}      = require('fs'),
+      RE_replace_template = /\$\{(\w+(?:\.\w+)*)}/g;
+
+function generateBlankId() {
+    return () => '_:' + uuid.v1();
+}
+
+/**
+ * @param {string} query
+ * @param {object} param
+ * @returns {string}
+ */
+function replaceTemplate(query, param) {
+    return query.replace(RE_replace_template, (match, path) => {
+        let target = param, segments = path.split('.');
+        while (segments.length > 0) {
+            if (!target) return '';
+            target = target[segments.shift()];
+        }
+        return target;
+    });
+} // replaceTemplate
+
+/**
+ * @typedef {object} Record
+ *
+ * @param {object} record
+ * @returns {Record}
+ */
+function convertRecord(record) {
+    const result = {};
+    for (let key of record['keys']) {
+        const value = record['_fields'][record['_fieldLookup'][key]];
+        result[key] = value;
+    }
+    return result;
+} // convertRecord
+
+/**
+ * @param {neo4j.driver} driver
+ * @param {string} query
+ * @param {object} [param]
+ * @returns {Promise<Array<Record>>}
+ */
+async function fetchData(driver, query, param) {
+    const session = driver.session();
+    try {
+        const result = await session.run(query, param);
+        session.close();
+        return result['records'].map(convertRecord);
+    } catch (err) {
+        session.close();
+        throw err;
+    }
+} // fetchData
+
+/**
+ * @param {string} filename
+ * @returns {function(neo4j.driver, object): Promise<Array<Record>>}
+ */
+function loadQuery(filename) {
+    const query = readFileSync(joinPath(__dirname, 'queries', filename)).toString();
+    return (driver, param) => fetchData(driver, replaceTemplate(query, param), param);
+} // loadQuery
+
+module.exports = {
+    assert: new Assert('module.persistence.neo4j'),
+    lockProp,
+    isDefined, isTruthy, isFalsy,
+    isBoolean, isNumber, isString,
+    isFunction, isObject,
+    isArray, isIterable,
+    generateBlankId,
+    loadQuery,
+    hrt
+};
